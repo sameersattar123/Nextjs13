5a730146bcee092c76865fc53b642a7b8412913d
Sameer <sameersattar1111@gmail.com>
2023-08-23 09:02:22 +0500

    css files  commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css"
 
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.containe}>
+      <Link href="/">Sameer</Link>
+      <div className="">
+        {links.map((link) => {
+          return (
+            <Link key={link.id} href={link.url}> 
+              {link.title}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
